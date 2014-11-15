73405a787c9e9927c3f03fea3f7a3358d123c53f
d0kt0r0 <ogbornd@mcmaster.ca>
2014-11-15 13:39:19 -0500

    refactored JS for web interface into a file shared by multiple HTML interfaces

---
+++ browser.js
@@ -0,0 +1,32 @@
+function postSignal(name) {
+  var password = document.getElementById('password').value;
+  if(password == null || password == "") {
+    alert("You must enter a password to evaluate code.");
+  }
+  else {
+    $.post(name + "-" + password, function(x) {});
+  }
+}
+
+function openEditor(name) {
+  sharejs.open(name,'text',function(error,doc) {
+    if(error) console.log(error);
+    else {
+      var elem = document.getElementById(name);
+      elem.disabled = false;
+      doc.attach_textarea(elem);
+    }
+  });
+}
+
+function setup() {
+  openEditor('edit1');
+  openEditor('edit2');
+  openEditor('edit3');
+  openEditor('edit4');
+  openEditor('edit5');
+  openEditor('edit6');
+  openEditor('edit7');
+  openEditor('edit8');
+  openEditor('chat');
+}
