202715b1dd32d9cbb7238ee1dd262b6f311379be
ogbornd@mcmaster.ca <ogbornd@Davids-MacBook-Pro.local>
2014-12-01 15:44:01 -0500

    added option to translate newlines to spaces, and added options parsing

--- client.js
+++ client.js
@@ -1,15 +1,50 @@
 var stderr = process.stderr;
-
-// subscribe to 0mq socket with code to evaluate 
-var address = process.argv[2];
+var nopt = require('nopt');
 var zmq = require('zmq');
+
+var knownOpts = { 
+    "server" : [String, null],
+    "port" : [Number, null],
+    "newlines-as-spaces" : Boolean };
+var shortHands = {
+    "s" : ["--server"],
+    "p" : ["--port"],
+    "n" : ["--newlines-as-spaces"] };
+var parsed = nopt(knownOpts,shortHands,process.argv,2);
+
+if(parsed['server'] == null) {
+    console.log("Error: MUST specify address of server's downstream with --server");
+    console.log("Usage:");
+    console.log(" --server (-s) [address]   address of server's downstream");
+    console.log(" --port (-p) [number]      port on which to connect to server (default: 8001)");
+    console.log(" --newlines-as-spaces (-n) converts any received newlines to spaces on stdout");
+    process.exit(1);
+}
+
+var newlines = parsed['newlines-as-spaces'];
+if(newlines) {
+    stderr.write("converting any received newlines to spaces on stdout\n");
+}
+
+var server = parsed['server'];
+var port = parsed['port'];
+if(port == null) { port = 8001; }
+var address = "tcp://" + server + ":" + (port.toString());
+
 var sub = zmq.socket('sub');
 sub.on('error',function (err) {
   stderr.write("error event on socket: " + err);
 });
-sub.on('message',function (msg) { 
-  console.log(msg.toString());
-  stderr.write(msg.toString()+"\n");
+sub.on('message',function (msg) {
+  var s;
+  if(newlines) { // convert newline sequences to whitespace
+      s = msg.toString().replace(/\n|\n\r|\r\n|\r/g," ");
+  }
+  else { // leave newline sequences as is
+      s = msg.toString();
+  }
+  console.log(s);
+  stderr.write(s+"\n");
 });
 
 stderr.write("connecting to " + address + " as subscriber\n");
