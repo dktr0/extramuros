a3b52a2ba353057586262f9a454bd9b829393ce2
d0kt0r0 <ogbornd@mcmaster.ca>
2015-07-06 15:38:03 -0400

    merge from Ian and some cleanup

--- visuals.js
+++ visuals.js
@@ -1,68 +1,67 @@
-var dimensions = {}; 
-var screen = {}; 
 var i = {}; 
 var funct={}; 
-var canv ={}; 
-var position = {}; 
 var fadeRate = 100; 
-var canvasCounter=0; 
 
-/// Set up Visual Context 
+var w; // width of our drawing context
+var h; // height of our drawing context
+var c; // a canvas context to use in 
+var draw = function() {}; // redefined in performance, called every animation frame
+var event = function(e) {}; // redefined in performance, called with events from server 
+ 
 function setupVisuals() {
-	// get size of the screen and set the canvas to the same size
-	var g = $("#global");
-	dimensions.g = g; 
-	dimensions.gx=g.width();dimensions.gy=g.height(); 
-	$('<canvas>').attr({id: "gcanvas"}).css({zIndex: $('canvas').length + 3}).insertBefore('#global');
- 	  	$('#global').css({zIndex: $('canvas').length + 3}); 
- 	  	dimensions.gcanvas = document.getElementById("gcanvas");
-//  	  	alert(canvas0); 
-			dimensions.gcanvas.width=dimensions.gx; dimensions.gcanvas.height=dimensions.gy; 
-			canv.gcanvas = dimensions.gcanvas.getContext('2d'); 
-	
-	new JSvisual(); 
-	console.log('visuals are set'); 
-}; 
-
-/// 
-var JSvisual = function (){
-	var self = this; 
-	var tick = function () {
-		$.each(funct, function(k,v) {
-// 				console.log(v); 
-				eval(v); 
-// 				console.log(k); 		
-			}); 
-			requestAnimationFrame(tick); 
-	};
-	$('#edit1').keyup(function (event) {
-       if (event.keyCode == 13 && event.shiftKey) {
-			 	var e1 = $('#edit1').val(); 
-			 	eval(e1);     
-      }; 	  
-});
- tick();
+    // add a new canvas adjusted to display dimensions and store context for later use
+    $('<canvas>').attr({id: "gcanvas"}).css({zIndex: $('canvas').length + 3}).insertBefore('#global');
+    $('#global').css({zIndex: $('canvas').length + 3});
+    adjustCanvasDimensions();
+    c = document.getElementById('gcanvas').getContext('2d');
+    // and activate our animation callback function 'tick'
+    requestAnimationFrame(tick); // activate our animation callback function 'tick'
+    setupShiftEnter(); // now shift-enter in any textarea evaluates that as JS for livecoding of visuals
+    console.log('visuals are set');
 };
 
-// CLEAR CANVAS
-var clear = function() {canv.gcanvas.clearRect(0,0,dimensions.gx, dimensions.gy)}; 
-// FADE CANVAS
+function adjustCanvasDimensions() {
+    w = $("#global").width();
+    h = $("#global").height();
+    var canvas = document.getElementById("gcanvas");
+    canvas.width = w;
+    canvas.height = h;
+}
+    
+function tick() {
+    draw();
+    $.each(funct, function(k,v) { eval(v); });
+    requestAnimationFrame(tick);
+}
 
-var fade = function(x) {
-	if(x=="stop"){ /// 
-		canv.gcanvas.beginPath();
-		canv.gcanvas.fillStyle = "rgba(255,255,255,0.1)";	
-		canv.gcanvas.fillRect(0,0,dimensions.gx, dimensions.gy);
-		canv.gcanvas.closePath(); 
-		} else {
-		canv.gcanvas.beginPath();
-		canv.gcanvas.fillStyle = "rgba(255,255,255,0.1)";	
-		canv.gcanvas.fillRect(0,0,dimensions.gx, dimensions.gy);
-		canv.gcanvas.closePath(); 
-		setTimeout(function() {fade()}, fadeRate)
-		}
+function retick() { // useful if in livecoding an error crashes animation callback
+    requestAnimationFrame(tick);
+}
+
+function setupShiftEnter() {
+    $('#edit1').keyup(function (event) {
+	if (event.keyCode == 13 && event.shiftKey) {
+	    var e1 = $('#edit1').val();
+	    eval(e1);
 	};
+    });
+    $('#edit3').keyup(function (event) {
+	if(event.keyCode==13 && event.shiftKey) {
+	    var e3 = $('#edit3').val();
+	    eval(e3);
+	};
+    });
+}
 
+function clear () {c.clearRect(0,0,w,h)};
+
+function fade (x) {
+    if(x!="stop") setTimeout(function() {fade()},fadeRate);
+    c.beginPath();
+    c.fillStyle = "rgba(255,255,255,0.1)";	
+    c.fillRect(0,0,w,h);
+    c.closePath();
+}
 	
 /// Circle 
 var drawCircle = function(name, colour,lineWidth) {
@@ -82,23 +81,20 @@ var drawCircle = function(name, colour,lineWidth) {
 };
 
 var drawCircle0 = function(name,colour,lineWidth){
-	var position = {x:Math.random()*dimensions.gx, y:dimensions.gy*Math.random()}; 
-	canv.gcanvas.beginPath(); 
-	canv.gcanvas.arc(
+	var position = {x:Math.random()*w, y:w*Math.random()}; 
+	c.beginPath(); 
+	c.arc(
 		position.x,
 		position.y,  
 		50*Math.random(),	0,	2*Math.PI
 							);
-	canv.gcanvas.lineWidth=lineWidth; 
-	canv.gcanvas.strokeStyle = colour;
-	canv.gcanvas.stroke();
-	canv.gcanvas.closePath();  
-
+	c.lineWidth=lineWidth; 
+	c.strokeStyle = colour;
+	c.stroke();
+	c.closePath();  
 };
  
- 
- 
- var drawLine = function(name, colour,lineWidth) {
+var drawLine = function(name, colour,lineWidth) {
 	//  alert('line'); 
 	if(lineWidth == null) {lineWidth=0.5};
 	if(colour == null) {colour="black"};
@@ -116,21 +112,19 @@ var drawCircle0 = function(name,colour,lineWidth){
 };
 
 var drawLine0 = function(name, colour,lineWidth){
-	var position = {x:Math.random()*dimensions.gx, y:dimensions.gy*Math.random(), 
-	x0:Math.random()*dimensions.gx, y0:dimensions.gy*Math.random() }; 
-	canv.gcanvas.beginPath(); 
-	canv.gcanvas.moveTo(position.x,position.y); 
-	canv.gcanvas.lineTo(position.x0,position.y0); 
-	canv.gcanvas.lineWidth=lineWidth; 
-	canv.gcanvas.strokeStyle = colour;
-	canv.gcanvas.stroke(); 
-	canv.gcanvas.closePath(); 
-
+    var position = {x:Math.random()*w,y:h*Math.random(), 
+	x0:Math.random()*w, y0:h*Math.random() }; 
+	c.beginPath(); 
+	c.moveTo(position.x,position.y); 
+	c.lineTo(position.x0,position.y0); 
+	c.lineWidth=lineWidth; 
+	c.strokeStyle = colour;
+	c.stroke(); 
+	c.closePath(); 
 };
 
 
-
- var drawSquare = function(name, type, colour,size,lw) {
+var drawSquare = function(name, type, colour,size,lw) {
 //  alert('line'); 
 
 	if(name == null) {name = 'empty'};
@@ -143,7 +137,7 @@ var drawLine0 = function(name, colour,lineWidth){
 	if ((name in funct) == true) {
  		console.log("Already a function "+ name);   
  		if(type == "stop") {delete funct[name]} 
- 			else  {console.log(canv[name]);funct[name] ="drawSquare0('"+name+ "',"+"'"+type+"'"+","+"'"+colour+"'"+","+size+ "," +lw+ ")"};
+ 			else  {funct[name] ="drawSquare0('"+name+ "',"+"'"+type+"'"+","+"'"+colour+"'"+","+size+ "," +lw+ ")"};
  	  } 
  	  else {console.log("New function");
 			i[name] = 0; 	
@@ -155,24 +149,23 @@ var drawLine0 = function(name, colour,lineWidth){
 
 
 		
-var drawSquare0 = function(name, type, colour, size, lineWidth){
-	var screen = canv.gcanvas; 
+var drawSquare0 = function(name, type, colour, size, lineWidth) {
 		
 		if (type !== "solid") {
-		screen.beginPath(); 
-		screen.lineWidth = 1;  
-		screen.rect(dimensions.gx/2+Math.sin(i[name])*dimensions.gx/2,dimensions.gy/2+Math.cos(3.01*i[name])*dimensions.gy/2,size, size);
-		screen.lineWidth = 1; 
-		screen.strokeStyle=colour;
-		screen.stroke();
-		screen.closePath(); 
-		i[name]+=2*Math.PI/dimensions.gx; 
+		c.beginPath(); 
+		c.lineWidth = 1;  
+		c.rect(w/2+Math.sin(i[name])*w/2,h/2+Math.cos(3.01*i[name])*h/2,size, size);
+		c.lineWidth = 1; 
+		c.strokeStyle=colour;
+		c.stroke();
+		c.closePath(); 
+		i[name]+=2*Math.PI/w; 
 // 			console.log(i[name]);  
 
 		} else {
-		screen.fillStyle=colour; 
-		screen.fillRect(dimensions.gx/2+Math.sin(i[name])*dimensions.gx/2,dimensions.gy/2+Math.cos(i[name])*dimensions.gy/2,size, size);	
-		i[name] += 2*Math.PI/dimensions.gx*10; 
+		c.fillStyle=colour; 
+		c.fillRect(w/2+Math.sin(i[name])*w/2,h/2+Math.cos(i[name])*h/2,size, size);	
+		i[name] += 2*Math.PI/w*10; 
 		; 
 		}
 
