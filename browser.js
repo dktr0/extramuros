a0be03ba1de17b2cc446e676a9f76d6dd83a9e3a
d0kt0r0 <ogbornd@mcmaster.ca>
2015-07-08 16:16:17 -0400

    ctrl+shift+enter evaluates JS in all browsers

--- browser.js
+++ browser.js
@@ -1,5 +1,9 @@
 var ws;
 
+function Osc() {}
+jQuery.extend(Osc.prototype,jQuery.eventEmitter);
+var osc = new Osc();
+
 function setup() {
     window.WebSocket = window.WebSocket || window.MozWebSocket;
     var url = 'ws://' + location.hostname + ':1337';
@@ -13,9 +17,15 @@ function setup() {
     };
     ws.onmessage = function (m) {
 	var data = JSON.parse(m.data);
-	var address = data.address.substring(1);
-	eval( address + "(data.args)");
-	// ummm... we should check to make sure the function exists first!...
+	if(data.type == 'osc') {
+	    var address = data.address.substring(1);
+	    $(osc).trigger(address);
+	    eval( address + "(data.args)");
+	    // ummm... we should check to make sure the function exists first!...
+	}
+	if(data.type == 'js') {
+	    eval(data.code);
+	}
     };
     for(var x=1;x<=20;x++) openEditor('edit' + x.toString());
     openEditor('chat');
@@ -23,19 +33,29 @@ function setup() {
     setupVisuals();
 }
 
+function getPassword() {
+    var x = document.getElementById('password').value;
+    if(x == null || password == "") {
+	alert("You must enter a password to evaluate code.");
+	return null;
+    }
+    return x;
+}
+
 function evaluateBuffer(name) {
-  var password = document.getElementById('password').value;
-  if(password == null || password == "") {
-    alert("You must enter a password to evaluate code.");
-  }
-  else {
-    var msg = {
-	request: "eval",
-	bufferName: name,
-	password: password
-    };
-    ws.send(JSON.stringify(msg));
-  }
+    var password = getPassword();
+    if(password) {
+	var msg = { request: "eval", bufferName: name, password: password };
+	ws.send(JSON.stringify(msg));
+    }
+}
+
+function evaluateJavaScriptGlobally(code) {
+    var password = getPassword();
+    if(password) {
+	var msg = { request: "evalJS", code: code, password: password };
+	ws.send(JSON.stringify(msg));
+    }
 }
 
 function openEditor(name) {
@@ -53,13 +73,19 @@ function openEditor(name) {
 
 function setupKeyboardHandlers() {
     $('textarea').keypress(function (event) {
-	if(event.keyCode == 13 && event.ctrlKey) {
+	if(event.keyCode == 13 && event.shiftKey && event.ctrlKey) {
+	    // ctrl+shift+enter: evaluate buffer as Javascript through server
+	    event.preventDefault();
+	    var code = $(this).val();
+	    evaluateJavaScriptGlobally(code);
+	}
+	else if(event.keyCode == 13 && event.ctrlKey) {
 	    // ctrl+Enter: evaluate text as JavaScript in local browser
 	    event.preventDefault();
 	    var code = $(this).val();
 	    eval(code);
-	};
-	if(event.keyCode == 13 && event.shiftKey) {
+	}
+	else if(event.keyCode == 13 && event.shiftKey) {
 	    // shift+Enter: evaluate buffer globally through the server   
 	    event.preventDefault();
 	    evaluateBuffer(event.target.id);
